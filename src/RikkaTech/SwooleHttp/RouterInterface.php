237fe9a6b3f3d4f2fe8d4d1a3b6f08db83f886f5
lixworth <lixworth@gmail.com>
2021-08-31 04:28:46 +0800

    project init

---
+++ src/RikkaTech/SwooleHttp/RouterInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * RouterInterface.php
+ *
+ * @project RinoBot
+ * @author lixworth <lixworth@outlook.com>
+ * @copyright RinoBot
+ * @create 2021/8/26 20:48
+ */
+
+namespace RikkaTech\SwooleHttp;
+
+
+interface RouterInterface
+{
+
+}
---
+++ src/RikkaTech/SwooleHttp/Singleton.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Singleton.php
+ *
+ * @project swoole-httpserver
+ * @author lixworth <lixworth@outlook.com>
+ * @copyright swoole-httpserver
+ * @create 2021/8/31 4:27
+ */
+
+namespace RikkaTech\SwooleHttp;
+
+class Singleton
+{
+    private static ?self $instance = null;
+
+    public function __construct()
+    {
+        self::$instance = $this;
+    }
+
+    private static function make(): self
+    {
+        return new self;
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = self::make();
+        }
+        return self::$instance;
+    }
+
+    public static function setInstance(self $instance): void
+    {
+        self::$instance = $instance;
+    }
+
+    public static function reset(): void
+    {
+        self::$instance = null;
+    }
+}
